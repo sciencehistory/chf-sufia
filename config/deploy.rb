9c338ddb849bd2dd351598e4a18594b7eef5a6e4
Alicia Cozine <alicia@curationexperts.com>
2015-06-23 09:49:28 -0500

    uses disambiguated project name and directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'sufia'
+set :application, 'chf-sufia'
 set :scm, :git
 set :repo_url, 'https://github.com/chemheritage/chf-sufia.git'
 set :branch, 'master'
-set :deploy_to, '/opt/sufia'
+set :deploy_to, '/opt/sufia-project'
 set :format, :pretty
 set :log_level, :debug
 set :keep_releases, 5
