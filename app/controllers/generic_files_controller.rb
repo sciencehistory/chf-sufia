646d1d2b3bf374cc84da7fdb706c1b3d6fb4dbad
Anna Headley <anna3LC@gmail.com>
2015-07-16 08:39:13 -0400

    Put batch creation back at page load. Attempt to fix null batch depositor.

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -5,4 +5,11 @@ class GenericFilesController < ApplicationController
   self.presenter_class = GenericFilePresenter
   self.edit_form_class = GenericFileEditForm
 
+  # TODO This is a temporary override of sufia to fix #101
+  #      This can be removed once sufia has a solution and we upgrade or
+  #      batches are no longer used when sufia migrates to PCDM
+  # routed to /files/new
+  def new
+    @batch_id  = Batch.create.id
+  end
 end
