6e0964d77c4e9ec782eaf7d83eca56af4a1d816b
Anna Headley <anna3LC@gmail.com>
2015-08-18 12:20:31 -0400

    Remove vendor/bundle from linked_dirs; bundler uses shared by default.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/initializers/blacklight_initializer.rb', 'config/initializers/devise.rb', 'config/blacklight.yml', 'config/database.yml', 'config/fedora.yml', 'config/redis.yml', 'config/resque-pool.yml', 'config/secrets.yml', 'config/solr.yml')
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
