095c6a22dd1b7374006d7debdfa125d142079612
Anna Headley <anna3LC@gmail.com>
2015-11-05 13:43:44 -0500

    Double the upload file size limits to 1 GB, 2 GB. Fix #93

--- app/assets/javascripts/chf_fileupload.js
+++ app/assets/javascripts/chf_fileupload.js
@@ -1,6 +1,6 @@
-//500 MB  max file size 
-max_file_size = 500000000;
-max_file_size_str = "500 MB";
-//1000 MB max total upload size
-max_total_file_size = 1000000000;
-max_total_file_size_str = "1000 MB";
+//1 GB max file size
+max_file_size = 1000000000;
+max_file_size_str = "1 GB";
+//2 GB max total upload size
+max_total_file_size = 2000000000;
+max_total_file_size_str = "2 GB";
