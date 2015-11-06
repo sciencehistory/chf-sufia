a57fbd6ac8f740c09064c93ea02b2a315a2ed524
Anna Headley <anna3LC@gmail.com>
2015-11-06 15:46:24 -0500

    chufia

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SufiaAgain
+module Chufia
   class Application < Rails::Application
     
     config.generators do |g|
