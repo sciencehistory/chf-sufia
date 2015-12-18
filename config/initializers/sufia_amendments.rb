2818c419376ddc0af0de3795df49fbc33d753064
Anna Headley <anna3LC@gmail.com>
2015-12-18 14:30:03 -0500

    Remove 'all rights reserved'. Add provisional URIs for rightsstatements.org options. Fixes #132.

--- config/initializers/sufia_amendments.rb
+++ config/initializers/sufia_amendments.rb
@@ -36,8 +36,17 @@ Sufia.config do |config|
   }
 
   config.cc_licenses = {
+    'In Copyright' => 'http://rightsstatements.org/vocab/InC/1.0/',
+    'In Copyright - EU Orphan Work' => 'http://rightsstatements.org/vocab/InC­OW­EU/1.0/',
+    'In Copyright - Rights­holder(s) Unlocatable or Unidentifiable' => 'http://rightsstatements.org/vocab/InC­RUU/1.0/',
+    'In Copyright - Educational Use Permitted' => 'http://rightsstatements.org/vocab/InC­EDU/1.0/',
+    'In Copyright - Non­Commercial Use Permitted' => 'http://rightsstatements.org/vocab/InC­NC/1.0/',
+    'Out Of Copyright - Non­Commercial Use Only' => 'http://rightsstatements.org/vocab/OOC­NC/1.0/',
+    'No Copyright - Contractual Restrictions' => 'http://rightsstatements.org/vocab/NoC­CR/1.0/',
+    'No Copyright - Other Known Legal Restrictions' => 'http://rightsstatements.org/vocab/NoC­OKLR/1.0/',
+    'No Copyright -  United States' => 'http://rightsstatements.org/vocab/NoC-US/1.0/',
+    'No Known Copyright' => 'http://rightsstatements.org/vocab/NKC/1.0/',
     'Public Domain Mark 1.0' => 'http://creativecommons.org/publicdomain/mark/1.0/',
-    'All rights reserved' => 'All rights reserved'
   }
 
   config.divisions = [
