fb11f3112012d2e3a5bce83dc6b7b14a976221f2
Anna Headley <anna3LC@gmail.com>
2016-02-01 15:03:48 -0500

    Don't grab related files from upload batch. fixes #144

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -77,6 +77,14 @@ class GenericFile < ActiveFedora::Base
   has_and_belongs_to_many :additional_credit, predicate: ::RDF::URI.new("http://chemheritage.org/ns/hasCredit"), class_name: "Credit"
   accepts_nested_attributes_for :additional_credit, reject_if: :all_blank, allow_destroy: true
 
+  # chf edit 2016-02-01 ah
+  # Override this from sufia-models/app/models/concerns/sufia/generic_file/batches.rb
+  # It's meaningless to define related_files in terms of what sufia6 calls 'batches' ('UploadSets' in sufia 7)
+  # However, let's not lose track of the batch_id altgoether just in case we want it for some reason later.
+  def related_files
+    []
+  end
+
   private
 
     def set_default_metadata
