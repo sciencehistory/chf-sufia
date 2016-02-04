6a4a1edf2945fd1244d8ddec4e72ea81c119efb2
Anna Headley <anna3LC@gmail.com>
2016-02-04 12:41:22 -0500

    opac js pulls actual data from controller, refs #140

--- app/assets/javascripts/load_from_opac.js
+++ app/assets/javascripts/load_from_opac.js
@@ -1,13 +1,47 @@
 var chf = chf || {}
 
 chf.load_from_opac = function() {
+  // get bib number from form
+  var bibnum = chf.get_bib_num();
+  // TODO: get resource type from form
   // get json response.
-  var response = 'hello';
+  var request = new XMLHttpRequest();
+  request.open('GET', '/opac_data/' + bibnum + '.json', true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      data = JSON.parse(request.responseText);
+      data = JSON.stringify(data, null, 2);
+      chf.display_data(data);
+    } else {
+      // We reached our target server, but it returned an error
+      data = JSON.parse(request.responseText);
+      data = JSON.stringify(data, null, 2);
+      data = request.status + ': ' + data;
+      chf.display_data(data);
+    }
+  };
+
+  request.onerror = function() {
+    var data = 'Opac API connection error'
+    chf.display_data(data);
+  };
+
+  request.send();
+};
+
+chf.get_bib_num = function() {
+  //return '123';
+  return 'B10691054';
+};
+
+chf.display_data = function(data) {
   // insert into box below button.
   var data_box = document.createElement('div');
   data_box.setAttribute('class', 'well');
   var p = document.createElement('p');
-  p.textContent = response;
+  p.textContent = data;
   data_box.appendChild(p);
   document.getElementById('opac_data').appendChild(data_box);
 };
