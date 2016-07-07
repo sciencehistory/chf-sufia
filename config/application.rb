34f70c7330220a7860e61a87058259381f8fd37d
Anna Headley <anna3LC@gmail.com>
2016-07-07 10:41:30 -0400

    Upgrade rails from 4.2.4 to 4.2.5

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Chufia
   class Application < Rails::Application
-    
-    config.generators do |g|
-      g.test_framework :rspec,
-        spec: true,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
-
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
