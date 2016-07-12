4ae9362a3c22059b83c8fc1fcea6f625d74301de
Anna Headley <anna3LC@gmail.com>
2016-07-12 11:36:01 -0400

    Sufia 7 upgrade: Apply db migration; changes db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416194156) do
+ActiveRecord::Schema.define(version: 20160711174245) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150416194156) do
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "checksum_audit_logs", force: :cascade do |t|
-    t.string   "generic_file_id"
+    t.string   "file_set_id"
     t.string   "dsid"
     t.string   "version"
     t.integer  "pass"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20150416194156) do
     t.datetime "updated_at"
   end
 
-  add_index "checksum_audit_logs", ["generic_file_id", "dsid"], name: "by_pid_and_dsid"
+  add_index "checksum_audit_logs", ["file_set_id", "dsid"], name: "by_pid_and_dsid"
 
   create_table "content_blocks", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,28 @@ ActiveRecord::Schema.define(version: 20150416194156) do
     t.datetime "updated_at"
     t.string   "external_key"
   end
+
+  create_table "curation_concerns_operations", force: :cascade do |t|
+    t.string   "status"
+    t.string   "operation_type"
+    t.string   "job_class"
+    t.string   "job_id"
+    t.string   "type"
+    t.text     "message"
+    t.integer  "user_id"
+    t.integer  "parent_id"
+    t.integer  "lft",                        null: false
+    t.integer  "rgt",                        null: false
+    t.integer  "depth",          default: 0, null: false
+    t.integer  "children_count", default: 0, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "curation_concerns_operations", ["lft"], name: "index_curation_concerns_operations_on_lft"
+  add_index "curation_concerns_operations", ["parent_id"], name: "index_curation_concerns_operations_on_parent_id"
+  add_index "curation_concerns_operations", ["rgt"], name: "index_curation_concerns_operations_on_rgt"
+  add_index "curation_concerns_operations", ["user_id"], name: "index_curation_concerns_operations_on_user_id"
 
   create_table "domain_terms", force: :cascade do |t|
     t.string "model"
@@ -62,14 +84,14 @@ ActiveRecord::Schema.define(version: 20150416194156) do
   add_index "domain_terms_local_authorities", ["local_authority_id", "domain_term_id"], name: "dtla_by_ids1"
 
   create_table "featured_works", force: :cascade do |t|
-    t.integer  "order",           default: 5
-    t.string   "generic_file_id"
+    t.integer  "order",      default: 5
+    t.string   "work_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "featured_works", ["generic_file_id"], name: "index_featured_works_on_generic_file_id"
   add_index "featured_works", ["order"], name: "index_featured_works_on_order"
+  add_index "featured_works", ["work_id"], name: "index_featured_works_on_work_id"
 
   create_table "file_download_stats", force: :cascade do |t|
     t.datetime "date"
@@ -175,7 +197,7 @@ ActiveRecord::Schema.define(version: 20150416194156) do
   add_index "mailboxer_receipts", ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
 
   create_table "proxy_deposit_requests", force: :cascade do |t|
-    t.string   "generic_file_id",                       null: false
+    t.string   "work_id",                               null: false
     t.integer  "sending_user_id",                       null: false
     t.integer  "receiving_user_id",                     null: false
     t.datetime "fulfillment_date"
@@ -198,6 +220,25 @@ ActiveRecord::Schema.define(version: 20150416194156) do
 
   add_index "proxy_deposit_rights", ["grantee_id"], name: "index_proxy_deposit_rights_on_grantee_id"
   add_index "proxy_deposit_rights", ["grantor_id"], name: "index_proxy_deposit_rights_on_grantor_id"
+
+  create_table "qa_local_authorities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "qa_local_authorities", ["name"], name: "index_qa_local_authorities_on_name", unique: true
+
+  create_table "qa_local_authority_entries", force: :cascade do |t|
+    t.integer  "local_authority_id"
+    t.string   "label"
+    t.string   "uri"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "qa_local_authority_entries", ["local_authority_id"], name: "index_qa_local_authority_entries_on_local_authority_id"
+  add_index "qa_local_authority_entries", ["uri"], name: "index_qa_local_authority_entries_on_uri", unique: true
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
@@ -246,10 +287,21 @@ ActiveRecord::Schema.define(version: 20150416194156) do
 
   create_table "trophies", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "generic_file_id"
+    t.string   "work_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "uploaded_files", force: :cascade do |t|
+    t.string   "file"
+    t.integer  "user_id"
+    t.string   "file_set_uri"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "uploaded_files", ["file_set_uri"], name: "index_uploaded_files_on_file_set_uri"
+  add_index "uploaded_files", ["user_id"], name: "index_uploaded_files_on_user_id"
 
   create_table "user_stats", force: :cascade do |t|
     t.integer  "user_id"
@@ -258,6 +310,7 @@ ActiveRecord::Schema.define(version: 20150416194156) do
     t.integer  "file_downloads"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "work_views"
   end
 
   add_index "user_stats", ["user_id"], name: "index_user_stats_on_user_id"
@@ -297,6 +350,10 @@ ActiveRecord::Schema.define(version: 20150416194156) do
     t.datetime "groups_last_update"
     t.string   "linkedin_handle"
     t.string   "orcid"
+    t.string   "arkivo_token"
+    t.string   "arkivo_subscription"
+    t.binary   "zotero_token"
+    t.string   "zotero_userid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
@@ -310,5 +367,17 @@ ActiveRecord::Schema.define(version: 20150416194156) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "work_view_stats", force: :cascade do |t|
+    t.datetime "date"
+    t.integer  "work_views"
+    t.string   "work_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "work_view_stats", ["user_id"], name: "index_work_view_stats_on_user_id"
+  add_index "work_view_stats", ["work_id"], name: "index_work_view_stats_on_work_id"
 
 end
