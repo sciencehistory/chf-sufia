7aba3594ee85daf163dbc9639cf4ea9dfd719283
Anna Headley <anna3LC@gmail.com>
2016-07-12 11:49:36 -0400

    Sufia 7 upgrade: Generate GenericWork curation concern

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -1,4 +1,6 @@
 Sufia.config do |config|
+  # Injected via `rails g sufia:work GenericWork`
+  config.register_curation_concern :generic_work
   # Email recipient of messages sent via the contact form
   # config.contact_email = "repo-admin@example.org"
 
