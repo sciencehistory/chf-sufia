9e0a3de944408bd42a6aba4c14084b73c618d936
Anna Headley <anna3LC@gmail.com>
2016-07-14 14:12:32 -0400

    Sufia 7 upgrade: Delete work presenter; gets rid of circular import error.
    move logic to the form

--- app/forms/curation_concerns/generic_work_form.rb
+++ app/forms/curation_concerns/generic_work_form.rb
@@ -3,7 +3,85 @@
 module CurationConcerns
   class GenericWorkForm < Sufia::Forms::WorkForm
     self.model_class = ::GenericWork
-    self.terms += [:resource_type]
+
+    # this list of terms is used for:
+    #   allowing the fields to be edited
+    #   TODO: dry this up? was previous in a presenter. do something like
+    #   https://github.com/aic-collections/aicdams-lakeshore/blob/cf197cab2b2f65f0841cbc61573ed8ef7c576c48/app/presenters/work_presenter.rb?
+    self.terms += [:title, :identifier,
+      ].concat(Rails.configuration.makers.keys).concat(
+        [:date_of_work]).concat(
+        Rails.configuration.places.keys).concat(
+        [
+        :resource_type, :genre_string,
+        :medium,
+        :extent,
+        :language,
+        :description,
+        :inscription,
+        :subject,
+        :division,
+        :series_arrangement,
+        :physical_container,
+        :related_url,
+        :rights,
+        :rights_holder,
+        :credit_line,
+        :additional_credit,
+        :file_creator,
+        :admin_note,
+        ])
+
+    # Add a new list for creating form elements on the edit pages
+    #   (since we've combined many of the fields into 'maker')
+    def edit_field_terms
+      [:title, :identifier, :maker,
+        :date_of_work,
+        :place,
+        :resource_type, :genre_string,
+        :medium,
+        :extent,
+        :language,
+        :description,
+        :inscription,
+        :subject,
+        :division,
+        :series_arrangement,
+        :physical_container,
+        :related_url,
+        :rights,
+        :rights_holder,
+        :credit_line,
+        :additional_credit,
+        :file_creator,
+        :admin_note,
+      ]
+    end
+
+    # We need these as hidden fields or else data deletion doesn't work.
+    def hidden_field_terms
+      Rails.configuration.makers.keys.concat(Rails.configuration.places.keys)
+    end
+
+    # post-upload edit form has a "show more" button; we want
+    # to control order independently here.
+    def above_fold_terms
+      [:identifier,
+      :maker,
+      :date_of_work,
+      :resource_type,
+      :genre_string,
+      ]
+    end
+    def below_fold_terms
+      edit_field_terms - above_fold_terms - [:title]
+    end
+
+    # give form access to attributes methods so it can build nested forms.
+    delegate :date_of_work_attributes=, :to => :model
+    delegate :inscription_attributes=, :to => :model
+    delegate :additional_credit_attributes=, :to => :model
+
 
     include NestedAttrs
     include ApplicationHelper
@@ -26,15 +104,15 @@ module CurationConcerns
 
     protected
 
-      # Override HydraEditor::Form to treat nested attbriutes accordingly
-      def initialize_field(key)
-        if reflection = model_class.reflect_on_association(key)
-          raise ArgumentError, "Association ''#{key}'' is not a collection" unless reflection.collection?
-          build_association(key)
-        else
-          super
-        end
-      end
+#      # Override HydraEditor::Form to treat nested attbriutes accordingly
+#      def initialize_field(key)
+#        if reflection = model_class.reflect_on_association(key)
+#          raise ArgumentError, "Association ''#{key}'' is not a collection" unless reflection.collection?
+#          build_association(key)
+#        else
+#          super
+#        end
+#      end
 
     private
 
