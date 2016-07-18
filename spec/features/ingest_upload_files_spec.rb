4420ad959f4ab704163dcbd5710be93d4809f878
Anna Headley <anna3LC@gmail.com>
2016-07-18 16:24:59 -0400

    Sufia 7 upgrade: Get rid of link to deposit agreement.

--- spec/features/ingest_upload_files_spec.rb
+++ spec/features/ingest_upload_files_spec.rb
@@ -3,19 +3,11 @@ require 'rails_helper'
 RSpec.feature "Uploading files via web form", :type => :feature do
   background do
     sign_in FactoryGirl.create(:depositor)
-    click_link "Upload"
+    visit '/concern/generic_works/new'
   end
 
-  scenario "form does not have terms of service field" do
-    expect(page).not_to have_text "You must agree to Sufia's Deposit Agreement before starting your upload"
-    expect(page).not_to have_field "terms_of_service"
-  end
-
-  scenario "upload button is enabled, with no hover message", :js do
-    attach_file("files[]", "#{Rails.root.to_s}/spec/fixtures/image.png")
-    expect(page).not_to have_css("button#main_upload_start[disabled]")
-    find('#main_upload_start_span').hover
-    expect(page).not_to have_text "Please accept Deposit Agreement before you can upload."
+  scenario "Deposit agreement has been removed" do
+    expect(page).not_to have_text "By saving this work I agree to the Deposit Agreement"
   end
 
 end
