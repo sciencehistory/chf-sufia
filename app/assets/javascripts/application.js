9a5a2c2e68b419323fc9a33328a9ab9bf04b79e0
Anna Headley <anna3LC@gmail.com>
2017-01-24 14:24:34 -0500

    Small edits to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,12 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require turbolinks//
+//= require turbolinks
 // Required by Blacklight
 //= require blacklight/blacklight
 
