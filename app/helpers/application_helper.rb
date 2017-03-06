6e0d043c680d574183e7125fbbacf2d0802028e5
Anna Headley <anna3LC@gmail.com>
2017-03-06 13:29:06 -0500

    Transform rights uri to label for facet display, fixes #353

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # A Blacklight facet field helper_method; maps rights URI to String
+  # @param [String] facet field uri value
+  # @return [String] rights statement label
+  def license_label(id)
+    service = CurationConcerns::LicenseService.new
+    service.label(id)
+  end
 end
