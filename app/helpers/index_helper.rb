581b248bda300c10975b2934035f620cfe71463f
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-03-16 14:47:41 -0400

    sanitize work description on index output too

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -6,7 +6,14 @@ module IndexHelper
       text = field
     end
 
-    text = truncate(text, length: 400, separator: /\s/)
+    # sanitize. should have been sanitized on input, but just to
+    # be safe.
+    text = DescriptionSanitizer.new.sanitize(text).html_safe
+
+    # truncate, make sure to tell it not to escape again, we already have.
+    # TODO: This is not a good solution for HTML text, may truncate in the middle
+    # of a tag, have to fix.
+    text = truncate(text, escape: false, length: 400, separator: /\s/)
 
     # a sufia helper to turn URLs into links.
     iconify_auto_link(text)
