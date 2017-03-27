23753a6132b2482535bfe0c9ebb2858ee3349547
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-03-27 11:28:19 -0400

    validations prevent non-member filesets being saved as thumbnail or representative
    In the case of representative, known to mess up display logic in bad ways, see #394.
    In case of thumbnail, just a precaution, enforce what I think are intended semantics.

--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -12,4 +12,26 @@ class GenericWork < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
+  validate :legal_representative_id, :legal_thumbnail_id
+
+  protected
+
+  def legal_representative_id
+    return if representative_id.nil?
+    # This is known to cause problems with curation_concerns@608b02916cd7,
+    # doesn't make sense to have a representative that's not in members.
+    # But happened at least once anyway. Try to prevent it.
+    unless ordered_member_ids.include?(representative_id) && member_ids.include?(representative_id)
+      errors.add(:representative_id, "must be included in members and ordered_members")
+    end
+  end
+
+  def legal_thumbnail_id
+    return if thumbnail_id.nil?
+    # While this is NOT known to cause problems with curation_concerns@608b02916cd7,
+    # it does seem to violate the intentional semantics, so we'll prevent it.
+    unless ordered_member_ids.include?(thumbnail_id) && member_ids.include?(thumbnail_id)
+      errors.add(:thumbnail_id, "must be included in members and ordered_members")
+    end
+  end
 end
