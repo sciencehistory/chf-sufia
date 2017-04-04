3cf27fd058cd9681fe5fe77a72492b9d8a84ec91
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-04-04 16:49:37 -0400

    workaround sufia/hyrax doing weird things, to get blacklight_range_limit in

---
+++ config/initializers/sufia_catalog_search_builder_overrides.rb
@@ -0,0 +1,16 @@
+# Unclear how to customize the Sufia::CatalogSearchBuilder, which
+# we need to do for blacklight_range_limit.
+# https://github.com/projecthydra-labs/hyrax/issues/707
+
+# to_prepare used in case there's dev-mode class reloading of the
+# thing we're patching. But we try to check to not do it multiple
+# times in case there isn't.
+Rails.application.config.to_prepare do
+  # would like to look up the class dynamically in case it changes,
+  # Sufia does this weird making this the only way, Hyrax is somewhat
+  # less weird.
+  klass = CatalogController.new.search_builder_class
+  unless klass.ancestors.include? BlacklightRangeLimit::RangeLimitBuilder
+    klass.send(:include, BlacklightRangeLimit::RangeLimitBuilder)
+  end
+end
