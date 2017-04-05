1a8426d30f03fd076b16255fe9c806fec792d51f
Anna Headley <anna3LC@gmail.com>
2017-04-05 16:23:46 -0400

    Add better error handling to fileset #create_derivatives

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -6,11 +6,15 @@ class FileSet < ActiveFedora::Base
   def create_derivatives(filename)
     # use layer 0 to create our derivatives
     if self.class.image_mime_types.include? mime_type
-      # create a preview derivative for image assets
-      Hydra::Derivatives::ImageDerivatives.create(filename,
-                                                  outputs: [{ label: :preview, format: 'jpg', size: '1000x750>', url: derivative_url('jpeg'), layer: 0 }])
-      Hydra::Derivatives::ImageDerivatives.create(filename,
-                                                  outputs: [{ label: :thumbnail, format: 'jpg', size: '200x150>', url: derivative_url('thumbnail'), layer: 0 }])
+      begin
+        # create a preview derivative for image assets
+        Hydra::Derivatives::ImageDerivatives.create(filename,
+                                                    outputs: [{ label: :preview, format: 'jpg', size: '1000x750>', url: derivative_url('jpeg'), layer: 0 }])
+        Hydra::Derivatives::ImageDerivatives.create(filename,
+                                                    outputs: [{ label: :thumbnail, format: 'jpg', size: '200x150>', url: derivative_url('thumbnail'), layer: 0 }])
+      rescue e
+        Rails.logger.error "Derivatives creation failed for #{id}: #{e}"
+      end
     else
       # any other mime_type uses default behavior
       super
