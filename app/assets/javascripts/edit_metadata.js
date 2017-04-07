5ac42960c9562027293975c53e811370fa67c909
Anna Headley <anna3LC@gmail.com>
2017-04-07 16:06:30 -0400

    Remove js suppression of tab during autocomplete
    It's causing an error and I don't think the behavior I was trying to
    override is there anymore; I think previously tab allowed you to move
    off of a field without selecting the value you were on; now it does
    select the value first

--- app/assets/javascripts/edit_metadata.js
+++ app/assets/javascripts/edit_metadata.js
@@ -65,15 +65,7 @@ Blacklight.onLoad(function() {
   }
 
   function set_autocomplete($elem, authority) {
-    $elem
-        // don't navigate away from the field on tab when selecting an item
-        .bind( "keydown", function( event ) {
-            if ( event.keyCode === $.ui.keyCode.TAB &&
-                    $( this ).data( "autocomplete" ).menu.active ) {
-                event.preventDefault();
-            }
-        })
-        .autocomplete( autocomplete_opts( authority ));
+    $elem.autocomplete( autocomplete_opts( authority ));
   }
 
   // loop over the autocomplete fields and attach the
