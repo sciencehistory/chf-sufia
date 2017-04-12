1b19371439e2835437e972e1e7070f98ec75bf5c
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-04-12 15:55:18 -0400

    Icon large rights statement under download button
    It currently just uses Hevetica for now. Our whole site just uses
    Helvetica. It ain't super pretty, but I think it makes more sense
    to wait till we make this whole page pretty after soft launch, then
    micro-design this particular element on a page that is otherwise
    still pretty messy and without really style determined.

--- app/presenters/curation_concerns/generic_work_show_presenter.rb
+++ app/presenters/curation_concerns/generic_work_show_presenter.rb
@@ -11,5 +11,32 @@ module CurationConcerns
       :inscription, :date_of_work, :engraver, :printer_of_plates,
       :additional_title,
       to: :solr_document
+
+    def rights_icon(identifier = rights.first)
+      # schema allows multiple rights, we only use one.
+      # we have added our own metadata to the liceneses.yml that the CC
+      # class doens't really have accessors for, we'll kinda hack it.
+      case CurationConcerns::LicenseService.new.authority.find(identifier).fetch("category", nil)
+      when "in_copyright"
+        "rightsstatements-InC.Icon-Only.dark.svg"
+      when "no_copyright"
+        "rightsstatements-NoC.Icon-Only.dark.svg"
+      else
+        "rightsstatements-Other.Icon-Only.dark.svg"
+      end
+    end
+
+    def rights_url(identifier = rights.first)
+      # we use resolvable urls already
+      identifier
+    end
+
+    def rights_icon_label(identifier = rights.first)
+      # we have added our own metadata to the liceneses.yml that the CC
+      # class doens't really have accessors for, we'll kinda hack it.
+      CurationConcerns::LicenseService.new.authority.find(identifier).fetch("short_label_html", "").html_safe
+    end
+
+
   end
 end
