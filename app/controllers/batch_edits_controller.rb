f3226cbe704810fffcf75309735341fa8cbcbbd1
Anna Headley <anna3LC@gmail.com>
2017-04-18 08:33:02 -0400

    Update comments for clarity

--- app/controllers/batch_edits_controller.rb
+++ app/controllers/batch_edits_controller.rb
@@ -7,20 +7,23 @@ class BatchEditsController < ApplicationController
     BatchEditForm
   end
 
-  # nearly straight-up copy of sufia's method
+  # Pulled code from Sufia::BatchEditsControllerBehavior#update,
+  # changing `super` call to `update_works`.
+  # (behavior we need to override is in super's super)
   def update
     case params["update_type"]
     when "update"
-      update_works
+      update_works # changed from `super`
     when "delete_all"
       destroy_batch
     end
   end
 
-  # the bulk of this is from hydra-batch-edit, with jobs processing added in.
-  # I tried to add the jobs by overriding update_document, which would have
+  # Pulled code from Hydra::BatchEditBehavior#update
+  # and added jobs to propagate permissions and visibility to contained files.
+  # (I tried to add the jobs by overriding update_document, which would have
   # been a smaller change, but that required saving the object twice, since the
-  # object needs to be saved before it's passed to the jobs.
+  # object needs to be saved before it's passed to the jobs.)
   def update_works
     batch.each do |doc_id|
       obj = ActiveFedora::Base.find(doc_id, :cast=>true)
