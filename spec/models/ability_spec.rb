598bec4eec7030df14c5da123ae9d99f80f4362c
Anna Headley <anna3LC@gmail.com>
2017-04-25 15:03:57 -0400

    refactor ability spec

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,42 +2,44 @@ require 'rails_helper'
 require 'cancan/matchers'
 
 RSpec.describe Ability do
+  let(:staff) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:admin) }
+  let(:admin_role) { Role.create name: "admin" }
+  before do
+    admin_role.users << admin
+    admin_role.save
+  end
 
   describe "an unprivileged user" do
-    let(:user) { FactoryGirl.create(:user) }
-
     it "is not an admin" do
-      expect(user.admin?).not_to eq true
+      expect(staff.admin?).not_to eq true
     end
 
-    subject { user }
-    it { is_expected.not_to be_able_to(:read, Role) }
-    it { is_expected.not_to be_able_to(:add_user, Role) }
-    it { is_expected.not_to be_able_to(:remove_user, Role) }
-    it { is_expected.not_to be_able_to(:destroy, Role) }
-    it { is_expected.not_to be_able_to(:edit, Role) }
-    it { is_expected.not_to be_able_to(:create, Role) }
+    it "cannot manage Roles" do
+      expect(staff).not_to be_able_to(:read, Role)
+      expect(staff).not_to be_able_to(:add_user, Role)
+      expect(staff).not_to be_able_to(:remove_user, Role)
+      expect(staff).not_to be_able_to(:destroy, Role)
+      expect(staff).not_to be_able_to(:create, Role)
+      expect(staff).not_to be_able_to(:edit, Role)
+    end
+
   end
 
   describe "an admin user" do
-    before do
-      @admin_role = Role.create name: "admin"
-      @admin = FactoryGirl.create(:admin)
-      @admin_role.users << @admin
-      @admin_role.save
-    end
 
     it "is an admin" do
-      expect(@admin.admin?).to eq true
+      expect(admin.admin?).to eq true
     end
 
-    subject { @admin }
-    it { is_expected.to be_able_to(:read, Role) }
-    it { is_expected.to be_able_to(:add_user, Role) }
-    it { is_expected.to be_able_to(:remove_user, Role) }
-    it { is_expected.not_to be_able_to(:destroy, Role) }
-    it { is_expected.not_to be_able_to(:edit, Role) }
-    it { is_expected.not_to be_able_to(:create, Role) }
+    it "can manage users' Role membership" do
+      expect(admin).to be_able_to(:read, Role)
+      expect(admin).to be_able_to(:add_user, Role)
+      expect(admin).to be_able_to(:remove_user, Role)
+      expect(admin).not_to be_able_to(:destroy, Role)
+      expect(admin).not_to be_able_to(:create, Role)
+      expect(admin).not_to be_able_to(:edit, Role)
+    end
 
   end
 end
