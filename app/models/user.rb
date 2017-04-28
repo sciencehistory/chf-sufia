349c23a095bd4f99c4b75af3dc11e1d69f4d6d93
Anna Headley <anna3LC@gmail.com>
2017-04-28 10:48:45 -0400

    Remove 'registerable' option from devise, refs #163
    Disables account creation and links thereto.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
