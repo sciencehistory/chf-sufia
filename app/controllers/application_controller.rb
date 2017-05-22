f216be1445aed3151aab9a02668835eb7212572f
Anna Headley <anna3LC@gmail.com>
2017-05-22 15:09:18 -0400

    Remove duplicate code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,6 @@ class ApplicationController < ActionController::Base
   # Please be sure to impelement current_user and user_session. Blacklight depends on
   # these methods in order to perform user specific actions.
 
-  with_themed_layout '1_column'
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
