dca844141da05ff2f9621ba2223449c656625da0
Anna Headley <anna3LC@gmail.com>
2017-05-23 13:22:54 -0400

    Implement new design for collections index, refs #547
    TODO:
    - With current sizes there is no way to ever get 4 columns. Think about
      ways to allow 4 columns; maybe set container width proportionally?
    - Justify center makes the elements in the last row come out of their
      columns. Justify left makes the entire flex area shift to the left of
      the page. Is there a way to make flexbox do what we want here or do we
      really need a grid?

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,4 +6,10 @@ class CollectionsController < ApplicationController
     CollectionEditForm
   end
 
+  def index
+    documents = super
+    @presenters = documents.
+      map { |document| presenter_class.new(document, current_ability) }.
+      sort! { |x,y| x.title.first <=> y.title.first }
+  end
 end
