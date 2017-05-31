87c81152584604dfed4dac43f7b672fa529f3481
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-05-31 14:53:29 -0400

    custom local FileSetPresenter

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -9,3 +9,9 @@ CurationConcerns.configure do |config|
   end
 end
 
+# We're actually overriding from the sufia-set Sufia::FileSetPresenter, to our
+# own. to_prepare seems necessary to make it stick in dev.
+Rails.application.config.to_prepare do
+  CurationConcerns::MemberPresenterFactory.file_presenter_class = CHF::FileSetPresenter
+end
+
