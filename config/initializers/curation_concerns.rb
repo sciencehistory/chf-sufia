f08f83c592238853094aaf6ab5bbdbc224c30afc
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-06-06 16:48:06 -0400

    move MemberPresenterFactory settings to an _override file

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -9,13 +9,3 @@ CurationConcerns.configure do |config|
   end
 end
 
-# We're actually overriding from the sufia-set Sufia::FileSetPresenter, to our
-# own. to_prepare seems necessary to make it stick in dev.
-Rails.application.config.to_prepare do
-  CurationConcerns::MemberPresenterFactory.file_presenter_class = CHF::FileSetPresenter
-  # And we also need to set the work_presenter_class, so it comes back from
-  # GenericWork#member_presenters. This is actually a custom CHF one despite the
-  # namespace.
-  CurationConcerns::MemberPresenterFactory.work_presenter_class = CurationConcerns::GenericWorkShowPresenter
-end
-
