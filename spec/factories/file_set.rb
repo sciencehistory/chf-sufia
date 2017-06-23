187bc452bd017f360793b669f2f5630e355795fd
Anna Headley <anna3LC@gmail.com>
2017-06-23 16:04:48 -0400

    remove unused file set factories

--- spec/factories/file_set.rb
+++ spec/factories/file_set.rb
@@ -24,37 +24,5 @@ FactoryGirl.define do
       read_groups ["registered"]
     end
 
-    factory :public_pdf do
-      transient do
-        id "fixturepdf"
-      end
-      initialize_with { new(id: id) }
-      read_groups ["public"]
-      resource_type ["Dissertation"]
-      subject %w(lorem ipsum dolor sit amet)
-      title ["fake_document.pdf"]
-      before(:create) do |fs|
-        fs.title = ["Fake PDF Title"]
-      end
-    end
-    factory :public_mp3 do
-      transient do
-        id "fixturemp3"
-      end
-      initialize_with { new(id: id) }
-      subject %w(consectetur adipisicing elit)
-      title ["Test Document MP3.mp3"]
-      read_groups ["public"]
-    end
-    factory :public_wav do
-      transient do
-        id "fixturewav"
-      end
-      initialize_with { new(id: id) }
-      resource_type ["Audio", "Dataset"]
-      read_groups ["public"]
-      title ["Fake Wav File.wav"]
-      subject %w(sed do eiusmod tempor incididunt ut labore)
-    end
   end
 end
