7e0f8703e50d9ed0079eca6cac0065e41e73d994
Anna Headley <anna3LC@gmail.com>
2017-06-26 10:09:57 -0400

    rename riiif_image_url to riiif_info_url for clarity

--- app/helpers/riiif_helper.rb
+++ app/helpers/riiif_helper.rb
@@ -1,6 +1,6 @@
 module RiiifHelper
   # use relative url unless we've defind a riiif server in config/environments/*.rb
-  def riiif_image_url (riiif_file_id)
+  def riiif_info_url (riiif_file_id)
     path = riiif.info_path(riiif_file_id, locale: nil)
     if Rails.configuration.respond_to? :riiif_server
       return URI.join(Rails.configuration.riiif_server, path).to_s
