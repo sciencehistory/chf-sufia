607c02bc6efe9463a97e23170af831566065125f
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-06-26 16:00:04 -0400

    download links in viewer

--- app/assets/javascripts/chf_image_viewer.js
+++ app/assets/javascripts/chf_image_viewer.js
@@ -112,12 +112,14 @@ ChfImageViewer.prototype.selectThumb = function(thumbElement) {
   $('.viewer-thumbs .viewer-thumb-selected').removeClass('viewer-thumb-selected')
   thumbElement.classList.add('viewer-thumb-selected');
 
-  var id    = thumbElement.getAttribute('data-member-id');
-  var index = thumbElement.getAttribute('data-index');
-  var shouldShowInfo = thumbElement.getAttribute('data-member-should-show-info') == "true";
-  var title = thumbElement.getAttribute('data-title');
-  var linkUrl   = thumbElement.getAttribute('data-member-show-url');
-  var tileSource = thumbElement.getAttribute('data-tile-source');
+  var id                    = thumbElement.getAttribute('data-member-id');
+  var index                 = thumbElement.getAttribute('data-index');
+  var shouldShowInfo        = thumbElement.getAttribute('data-member-should-show-info') == "true";
+  var title                 = thumbElement.getAttribute('data-title');
+  var linkUrl               = thumbElement.getAttribute('data-member-show-url');
+  var downloadOriginalUrl   = thumbElement.getAttribute('data-member-dl-original-url');
+  var downloadJpegUrl       = thumbElement.getAttribute('data-member-dl-jpeg-url');
+  var tileSource            = thumbElement.getAttribute('data-tile-source');
 
   $('.viewer-image').addClass('viewer-image-loading');
 
@@ -126,6 +128,11 @@ ChfImageViewer.prototype.selectThumb = function(thumbElement) {
   document.querySelector('*[data-hook="viewer-navbar-title-label"]').textContent = title;
   document.querySelector('*[data-hook="viewer-navbar-info-link"]').href = linkUrl;
   document.getElementsByClassName('viewer-pagination-numerator').item(0).textContent = index;
+
+  var originalLink = document.querySelector('#chf-image-viewer *[data-content-hook=dl-original-link]');
+  if (originalLink) { originalLink.setAttribute('href', downloadOriginalUrl); }
+  var jpegLink = document.querySelector('#chf-image-viewer *[data-content-hook=dl-jpeg-link]');
+  if (jpegLink) { jpegLink.setAttribute('href', downloadJpegUrl); }
 
   if (shouldShowInfo) {
     // spacer shows up when info doesn't.
