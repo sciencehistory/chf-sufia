b2e11ac8813bf5d07d2e8be0b97581e352428791
Anna Headley <anna3LC@gmail.com>
2017-06-29 12:18:56 -0400

    Raise if public_riiif_url is not a valid host

--- spec/helpers/riiif_helper_spec.rb
+++ spec/helpers/riiif_helper_spec.rb
@@ -18,10 +18,19 @@ describe RiiifHelper do
     end
 
     context "riiif configured for remote box" do
-      it "provides full url" do
-        allow(CHF::Env).to receive(:lookup).with(:public_riiif_url).and_return('//example.com')
-        expect(helper.riiif_info_url('file_id')).to eq "//example.com/image-service/file_id/info.json"
+      context "correct config supplied" do
+        it "provides full url" do
+          allow(CHF::Env).to receive(:lookup).with(:public_riiif_url).and_return('//example.com')
+          expect(helper.riiif_info_url('file_id')).to eq "//example.com/image-service/file_id/info.json"
+        end
+      end
+      context "bare host supplied" do
+        it "raises" do
+          allow(CHF::Env).to receive(:lookup).with(:public_riiif_url).and_return('example.com')
+          expect{ helper.riiif_info_url('file_id') }.to raise_error(RuntimeError)
+        end
       end
     end
   end
+
 end
