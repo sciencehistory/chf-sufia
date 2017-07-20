420a9fc2ddea4c678d99f741750c3956ada4f8f7
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-07-19 21:20:10 -0400

    change _show_page_image_file_set_actions partial to helper
    for performance. Makes a big difference when called once-per-member on a show page with many members

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -1,6 +1,11 @@
 # helpers for rendering things related to display of 'members' or children
 # of works, on a work page. A member can be a file set or a child work.
 module MemberHelper
+
+  def dropdown_menuitem(content, attributes = {})
+    content_tag("li", content, {tabindex: "-1", role: "menuitem"}.merge(attributes))
+  end
+
   # returns a <ul> with <li>s suitable to be used inside a bootstrap
   # dropdown-toggle, representing download options for the member passed in.
   # also includes a rights statement from parent.
@@ -62,5 +67,52 @@ module MemberHelper
     end
 
     content_tag("ul", safe_join(list_elements), class: "dropdown-menu")
+  end
+
+  # Used for controls (edit etc) on a 'file_set', normally only showed
+  # to staff users.  Used in show_page_image.
+  #
+  # returns a <ul> with <li>s suitable to be used inside a bootstrap
+  # dropdown-toggle.
+  #
+  # Originally was a partial instead of a helper, and probably more readable
+  # that way, but performance impact of partial was too much, on work pages
+  # that wanted to display many members and call these many times. Not entirely
+  # sure why partial so much slower than helper, even in production config.
+  def file_set_actions_menu(file_set, parent:, labelled_by: nil)
+    list_items = []
+
+    list_items << dropdown_menuitem(
+      link_to 'File Detail', contextual_path(file_set, parent),
+               title: file_set.to_s.inspect, target: "_blank"
+    )
+
+    list_items  << '<li class="divider"></li>'.html_safe
+
+    if can?(:edit, file_set.id)
+      list_items << dropdown_menuitem(
+        link_to 'Edit', edit_polymorphic_path([main_app, file_set]),
+                { title: "Edit #{file_set}" }
+      )
+
+      list_items << dropdown_menuitem(
+        link_to 'Versions',  edit_polymorphic_path([main_app, file_set], anchor: 'versioning_display'),
+                { title: "Display previous versions" }
+      )
+    end
+
+    if can?(:destroy, file_set.id)
+      list_items << dropdown_menuitem(
+        link_to 'Delete', polymorphic_path([main_app, file_set]),
+                 method: :delete, title: "Delete #{file_set}",
+                 data: {confirm: "Deleting #{file_set} from #{application_name} is permanent. Click OK to delete this from #{application_name}, or Cancel to cancel this operation"}
+      )
+    end
+
+    content_tag("ul",
+                safe_join(list_items),
+                class: "dropdown-menu dropdown-menu-right",
+                role: "menu",
+                :'aria-labelledby' => labelled_by)
   end
 end
