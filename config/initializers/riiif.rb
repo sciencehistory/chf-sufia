a6c63a86f6a54e0d5ffcd038e204cf7843cf8b5d
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-07-24 13:25:18 -0400

    Don't set riiif external_command configs unless we actually have something to set it to

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -6,8 +6,12 @@ Riiif::Image.cache = ActiveSupport::Cache::FileStore.new(CHF::Env.lookup(:riiif_
 
 # If these are nil / not set, ImageMagick commands will be used
 # set to "gm convert" and "gm identify" to use GraphicsMagick
-Riiif::ImagemagickCommandFactory.external_command = CHF::Env.lookup(:riiif_convert_command)
-Riiif::ImageMagickInfoExtractor.external_command  = CHF::Env.lookup(:riiif_identify_command)
+if CHF::Env.lookup(:riiif_convert_command).present?
+  Riiif::ImagemagickCommandFactory.external_command = CHF::Env.lookup(:riiif_convert_command)
+end
+if CHF::Env.lookup(:riiif_identify_command).present?
+  Riiif::ImageMagickInfoExtractor.external_command  = CHF::Env.lookup(:riiif_identify_command)
+end
 
 # Tell RIIIF how to resolve the identifier to a URI in Fedora
 Riiif::Image.file_resolver.id_to_uri = lambda do |id|
