2d11554eeee0a361470974944348b2cf5ad8bd0a
Anna Headley <anna3LC@gmail.com>
2017-07-31 09:17:12 -0400

    Add helper test for cantaloupe url

--- spec/helpers/image_service_helper_spec.rb
+++ spec/helpers/image_service_helper_spec.rb
@@ -15,9 +15,15 @@ describe ImageServiceHelper do
 
     context "configured for remote box" do
       context "correct config supplied" do
-        it "provides full url" do
+        before do
           allow(CHF::Env).to receive(:lookup).with(:public_riiif_url).and_return('//example.com')
+        end
+        it "provides full riiif url" do
           expect(helper.iiif_info_url('file_id')).to eq "//example.com/image-service/file_id/info.json"
+        end
+        it "provides full cantaloupe url" do
+          allow(CHF::Env).to receive(:lookup).with(:image_server).and_return('cantaloupe')
+          expect(helper.iiif_info_url('file_id')).to eq "//example.com/iiif/2/file_id/info.json"
         end
       end
       context "bare host supplied" do
