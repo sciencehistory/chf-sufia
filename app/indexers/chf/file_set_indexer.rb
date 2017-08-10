e743c104b3bd22b81788e2997dd27958c90aa72b
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-08-10 12:04:27 -0400

    Index representative_checksum too, for fingerprinted URLs

--- app/indexers/chf/file_set_indexer.rb
+++ app/indexers/chf/file_set_indexer.rb
@@ -4,6 +4,7 @@ module CHF
       super.tap do |doc|
         if object.original_file
           doc[ActiveFedora.index_field_mapper.solr_name('original_file_id')] = object.original_file.id
+          doc[ActiveFedora.index_field_mapper.solr_name('original_file_checksum')] = object.original_file.checksum.value
         end
       end
     end
