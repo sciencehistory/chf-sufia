bf5af72f3e9594288a3e51aaabd15dad62911a58
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-08-31 12:13:55 -0400

    no longer need this with new version of aws-sdk we already upgraded

--- config/initializers/aws_sdk.rb
+++
@@ -1,5 +0,0 @@
-# Were previously having some weird NameErrors in resque jobs.
-# https://github.com/aws/aws-sdk-ruby/issues/1233
-if Rails.env.production? && defined?(Aws)
-  Aws.eager_autoload!(services: ['S3'])
-end
