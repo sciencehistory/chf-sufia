e532a55b1abae3f13e90d02d58668f38feeb3323
Anna Headley <anna3LC@gmail.com>
2017-09-05 11:35:11 -0400

    Fix comment that went out of date

--- app/models/search_builder/public_domain_filter.rb
+++ app/models/search_builder/public_domain_filter.rb
@@ -7,7 +7,7 @@ class SearchBuilder
     end
 
     def public_domain_filter(solr_params)
-      # look for the checkbox; transform it into a facet param
+      # look for the public domain only checkbox
       if blacklight_params.fetch("filter_public_domain", 0).to_i > 0
         solr_params[:fq] ||= []
         public_domain_filter = "{!term f=rights_sim}http://creativecommons.org/publicdomain/mark/1.0/"
