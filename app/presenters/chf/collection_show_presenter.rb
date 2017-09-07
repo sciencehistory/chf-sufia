d9eaa2066d0facfd129246f4f9c25cc07b7da689
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-09-07 14:48:45 -0400

    collection pages in search

--- app/presenters/chf/collection_show_presenter.rb
+++ app/presenters/chf/collection_show_presenter.rb
@@ -1,12 +1,16 @@
 module CHF
   class CollectionShowPresenter < Sufia::CollectionPresenter
+    # object-less class function so we can use it over in Blackight-based views
+    # and presenters insteed of this sufia-based one. :(
+    def self.thumbnail_src(solr_document, default: 'default_collection.svg')
+      relative_path = solr_document.first(Solrizer.solr_name('representative_image_path', :displayable)).presence || default
+      "collections/#{relative_path}" if relative_path
+    end
 
     # returns arg that can be passed to 'image_tag'
     # will return default image if needed, or pass `default: nil` or your own path
     def thumbnail_src(default: 'default_collection.svg')
-      relative_path = self.solr_document.first(Solrizer.solr_name('representative_image_path', :displayable)).presence || default
-      "collections/#{relative_path}" if relative_path
+      self.class.thumbnail_src(self.solr_document, default: default)
     end
-
   end
 end
