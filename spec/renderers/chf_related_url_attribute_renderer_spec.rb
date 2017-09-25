1de13326620be96fbeb6c61a04d14198e94408b5
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-09-25 14:50:29 -0400

    fix spec for new rendering func

--- spec/renderers/chf_related_url_attribute_renderer_spec.rb
+++ spec/renderers/chf_related_url_attribute_renderer_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 require 'rspec/matchers' # req by equivalent-xml custom matcher `be_equivalent_to`
 require 'equivalent-xml/rspec_matchers'
 
-RSpec.describe BlankTargetAttributeRenderer do
+RSpec.describe ChfRelatedUrlAttributeRenderer do
   let(:field) { :related_url }
-  let(:renderer) { described_class.new(field, ['http://www.example.com']) }
+  let(:renderer) { described_class.new(field, ['http://www.example.com/something']) }
 
   describe "#attribute_to_html" do
     subject { Nokogiri::HTML(renderer.render) }
     let(:expected) { Nokogiri::HTML(tr_content) }
 
     let(:tr_content) {
-      "<tr><th>Related url</th>\n" \
-        "<td><ul class=\"tabular\"><li class=\"attribute related_url\"><a target=\"_blank\" href=\"http://www.example.com\"><span class=\"glyphicon glyphicon-new-window\"></span>&nbsp;http://www.example.com</a></li></ul></td>\n" \
+      "<tr><th>Related url</th>" \
+        "<td><ul class=\"tabular\"><li class=\"attribute related_url\"><a target=\"_blank\" href=\"http://www.example.com/something\"><span class=\"glyphicon glyphicon-new-window\"></span>\u00A0www.example.com/...</a></li></ul></td>" \
         "</tr>"
     }
     it { expect(renderer).not_to be_microdata(field) }
