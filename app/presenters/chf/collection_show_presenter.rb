639f19c866a9ee50ca21fcacc8e874045483e2e1
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-09-26 18:07:36 -0400

    collection show page metadata attributes

--- app/presenters/chf/collection_show_presenter.rb
+++ app/presenters/chf/collection_show_presenter.rb
@@ -16,5 +16,14 @@ module CHF
     def needs_permission_badge?
       solr_document.visibility != Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
     end
+
+    def urls_to_catalog
+      @urls_to_catalog ||= @solr_document.related_url.find_all do |url|
+        url.start_with? "http://othmerlib.chemheritage.org/record="
+      end
+    end
+
+
+
   end
 end
