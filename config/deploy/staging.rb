9b71688f0deeab5cf686665d5590e92e6eea19e6
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-10-24 15:17:35 -0400

    new jobs-stage numeric IP

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :stage, :staging
 set :rails_env, 'production'
 server 'staging.digital.chemheritage.org', user: 'hydep', roles: [:web, :app, :db, :jobs]
 server '184.73.72.239', user: 'hydep', roles: [:solr]
-server '52.87.242.20', user: 'hydep', roles: [:jobs] # dzi bg job box
+server '54.204.60.1', user: 'hydep', roles: [:jobs] # dzi bg job box
 
 
 # server-based syntax
