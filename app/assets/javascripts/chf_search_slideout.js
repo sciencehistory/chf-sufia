5e4bcc47bffe78c89177566a4833561206647af7
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-12-20 12:34:04 -0500

    search slider stays down with no sliding when in collapsed navbar

--- app/assets/javascripts/chf_search_slideout.js
+++ app/assets/javascripts/chf_search_slideout.js
@@ -6,25 +6,46 @@ function ChfSearchSlideout(element) {
   this.element.on("click touch hover focusin", function() {
     _self.open();
   });
-}
+};
 
 ChfSearchSlideout.prototype.animationTime = 180;
 
 ChfSearchSlideout.prototype.drawerElement = function() {
   return $(this.element).find('.search-options');
-}
+};
 
 ChfSearchSlideout.prototype.open = function() {
   this.element.removeClass("closed").addClass("opened");
   this.installLeaveHandlers();
   this.drawerElement().slideDown(this.animationTime);
-}
+};
 
 ChfSearchSlideout.prototype.close = function() {
+  if (this.forceExpanded) {
+    return;
+  }
+
   this.element.removeClass("opened").addClass("closed");
- this.drawerElement().slideUp(this.animationTime);
- this.removeLeaveHandlers();
-}
+  this.drawerElement().slideUp(this.animationTime);
+  this.removeLeaveHandlers();
+};
+
+ChfSearchSlideout.prototype.navbarBreakpoint = function() {
+  return 739;
+};
+
+ChfSearchSlideout.prototype.onResize = function() {
+  if ( $(window).width() <= this.navbarBreakpoint()) {
+    this.element.addClass("force-expanded");
+    if (this.element.hasClass("closed")) {
+      this.open();
+    }
+    this.forceExpanded = true;
+  } else {
+    this.element.removeClass("force-expanded");
+    this.forceExpanded = false;
+  }
+};
 
 ChfSearchSlideout.prototype.installLeaveHandlers = function() {
   var _self = this;
@@ -39,14 +60,18 @@ ChfSearchSlideout.prototype.installLeaveHandlers = function() {
       _self.close();
     }
   });
-}
+};
 
 ChfSearchSlideout.prototype.removeLeaveHandlers = function() {
   this.element.off(".chfSearchSlideout");
   $("body").off(".chfSearchSlideout");
-}
+};
 
 
 jQuery(document).ready(function($) {
-  new ChfSearchSlideout($(".masthead .nav-search-form"));
+  var slideout = new ChfSearchSlideout($(".masthead .nav-search-form"));
+  // If navbar is collapsed, put search slider dropdown down.
+  $(window).on("load resize", function(e) {
+    slideout.onResize();
+  });
 });
