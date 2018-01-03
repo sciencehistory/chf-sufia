869348c56f6e6f29006c3e5ef0c024bc9612ab2b
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-01-03 10:45:59 -0500

    basic search smoke test

---
+++ spec/features/search_feature_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+# Some top-level 'smoke tests' to make sure the app is working.
+
+# Feature tests are really slow always, and even slower cause it's so slow
+# for us to create records in samvera. So we pack a bunch of stuff
+# into each scenario, even though that's often said to be not great test design.
+
+RSpec.feature "Search smoke-tests", js: true do
+  let!(:record1) { FactoryGirl.create(:full_public_work, title: ["Record One"]) }
+  let!(:record2) { FactoryGirl.create(:full_public_work, title: ["Record Two"]) }
+
+  context("as a not-logged in user") do
+    scenario "searching" do
+      visit(root_path)
+      fill_in 'q', with: 'two'
+      click_button 'Search'
+
+      # Results page
+      expect(page).to have_current_path(search_catalog_path, only_path: true)
+      expect(page).to have_css(".page_links", text: "1 entry found")
+      expect(page).to have_css("li.document", count: 1)
+      click_link "Record Two"
+
+      # Record Two show page, check for some features on page
+      expect(page).to have_current_path(curation_concerns_generic_work_path(record2.id), only_path: true)
+      expect(page).to have_text(record2.genre_string.join(","))
+      expect(page).to have_text(record2.title.first)
+      expect(page).to have_link(text: record2.author.first, href: search_catalog_path(f: {maker_facet_sim: [record2.author.first]}))
+
+      # image is there. too hard to check actual src url at present.
+      expect(page).to have_css("img.show-page-image-image")
+
+      # download menu opens
+      click_button "Download"
+      expect(page).to have_css(".dropdown-menu.download-menu")
+      expect(page).to have_link("Original file")
+    end
+  end
+
+end
