95fb0ae7e2bf8bff8a21f484a5a08ed1e832354d
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-01-18 11:06:01 -0500

    generated sitemap includes images

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,6 +4,8 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'https://digital.sciencehistory.org'
 
 SitemapGenerator::Sitemap.create do
+  image_service_class = ImageServiceHelper.image_url_service_class(CHF::Env.lookup(:image_server_for_thumbnails))
+
   add about_path, changefreq: 'monthly'
   add policy_path, changefreq: 'monthly'
   add faq_path, changefreq: 'monthly'
@@ -14,15 +16,22 @@ SitemapGenerator::Sitemap.create do
   read_solr_field = Solrizer.solr_name('read_access_group', :symbol)
 
   GenericWork.find_each(read_solr_field => 'public') do |w|
+    presenter = CurationConcerns::GenericWorkShowPresenter.new(w, Ability.new(nil))
+
+    # spec says max 1000 images.
+    image_urls = presenter.viewable_member_presenters.slice(0, 1000).collect do |member|
+      image_service = image_service_class.new(file_set_id: member.representative_file_set_id, file_id: member.representative_file_id, checksum: member.representative_checksum)
+      image_service.thumb_url(size: :large, density_descriptor: "2X")
+    end
+
     add curation_concerns_generic_work_path(w),
         changefreq: 'monthly',
-        lastmod: nil
+        lastmod: nil,
+        images: image_urls.collect { |url| { loc: url } }
   end
 
   Collection.find_each(read_solr_field => 'public') do |c|
     add collection_path(c), changefreq: 'weekly', lastmod: nil
   end
-
-
 end
 
