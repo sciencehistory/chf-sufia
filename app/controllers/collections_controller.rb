d3734522024ad3c1adbcad7eff15f44b48ee47bc
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-02-05 15:44:33 -0500

    override CollectionsController to allow facet limits

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -29,4 +29,21 @@ class CollectionsController < ApplicationController
     @parent_presenter_lookup = parent_lookup_hash(@member_docs)
   end
 
+  protected
+
+  # Have to override method from curation_concerns to ignore facets when trying
+  # to fetch collection. gah.
+  # https://github.com/samvera/curation_concerns/blob/v1.7.8/app/controllers/concerns/curation_concerns/collections_controller_behavior.rb?utf8=%E2%9C%93#L177-L192
+  def single_item_search_builder
+    single_item_search_builder_class.new(self).with(params.except(:q, :page, :f))
+  end
+
+  # Have to override this helper method from Blacklight to tell it, no, keep the
+  # search HERE in this controller, don't go over to CatalogController. Ergh.
+  # https://github.com/projectblacklight/blacklight/blob/v6.7.2/app/controllers/concerns/blacklight/controller.rb#L71-L74
+  def search_action_url options = {}
+    url_for(options.except(:controller, :action, :q))
+  end
+  helper_method :search_action_url
+
 end
