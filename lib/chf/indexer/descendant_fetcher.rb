430833c207ad99dcc662d38580ea99c806f8356b
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-03-07 12:22:27 -0500

    Make sure Collections are indexed first

--- lib/chf/indexer/descendant_fetcher.rb
+++ lib/chf/indexer/descendant_fetcher.rb
@@ -6,7 +6,14 @@ module CHF
     #
     # https://github.com/projecthydra/active_fedora/pull/1219
     # https://github.com/projecthydra/active_fedora/pull/1218
-    if Gem.loaded_specs["active-fedora"].version >= Gem::Version.new('11.2')
+    #
+    # Except now we also need cjcolvar's descendant_and_self_uris_partitioned_by_model method,
+    # which we've incorporated here too.
+    # https://github.com/samvera/active_fedora/pull/1275
+    #
+    # Note we've also added `Collection` to `default_priority_models` to make sure they
+    # index first. Oddly, I had troble settig this class_attribute externally.
+    if Gem.loaded_specs["active-fedora"].version >= Gem::Version.new('12.0')
        msg = "\n\nPlease check and make sure this patch is still needed at #{__FILE__}:#{__LINE__}\n\n"
        $stderr.puts msg
        Rails.logger.warn msg
@@ -19,7 +26,7 @@ module CHF
     #
     # The DescendantFetcher is also capable of partitioning the URIs into "priority" URIs
     # that will be first in the returned list. These prioritized URIs belong to objects
-    # with certain hasModel models. This feature is used in some hydra apps that need to
+    # with certain hasModel models. This feature is used in some samvera apps that need to
     # index 'permissions' objects before other objects to have the solr indexing work right.
     # And so by default, the prioritized class names are the ones form Hydra::AccessControls,
     # but you can alter the prioritized model name list, or set it to the empty array.
@@ -35,16 +42,18 @@ module CHF
       HAS_MODEL_PREDICATE = ActiveFedora::RDF::Fcrepo::Model.hasModel
 
       class_attribute :default_priority_models, instance_accessor: false
-      self.default_priority_models = %w(Hydra::AccessControl Hydra::AccessControl::Permissions).freeze
+      self.default_priority_models = %w(Hydra::AccessControl Hydra::AccessControl::Permissions Collection).freeze
 
       attr_reader :uri, :priority_models
 
       def initialize(uri,
-                     priority_models: self.class.default_priority_models)
+                     priority_models: self.class.default_priority_models, exclude_self: false)
         @uri = uri
         @priority_models = priority_models
+        @exclude_self = exclude_self
       end
 
+      # @return [Array<String>] uris starting with priority models
       def descendant_and_self_uris
         partitioned = descendant_and_self_uris_partitioned
         partitioned[:priority] + partitioned[:other]
@@ -52,22 +61,34 @@ module CHF
 
       # returns a hash where key :priority is an array of all prioritized
       # type objects, key :other is an array of the rest.
+      # @return [Hash<String, Array<String>>] uris sorted into :priority and :other
       def descendant_and_self_uris_partitioned
-        resource = Ldp::Resource::RdfSource.new(ActiveFedora.fedora.connection, uri)
+        model_partitioned = descendant_and_self_uris_partitioned_by_model
+
+        { priority: model_partitioned.slice(*priority_models).values.flatten,
+          other: model_partitioned.slice(*(model_partitioned.keys - priority_models)).values.flatten }
+      end
+
+      # Returns a hash where keys are model names
+      # This is useful if you need to action on certain models and want finer grainularity than priority/other
+      # @return [Hash<String, Array<String>>] uris sorted by model names
+      def descendant_and_self_uris_partitioned_by_model
         # GET could be slow if it's a big resource, we're using HEAD to avoid this problem,
         # but this causes more requests to Fedora.
-        return partitioned_uris unless resource.head.rdf_source?
+        return partitioned_uris unless rdf_resource.head.rdf_source?
 
-        add_self_to_partitioned_uris
+        add_self_to_partitioned_uris unless @exclude_self
 
         immediate_descendant_uris = rdf_graph.query(predicate: ::RDF::Vocab::LDP.contains).map { |descendant| descendant.object.to_s }
         immediate_descendant_uris.each do |descendant_uri|
           self.class.new(
             descendant_uri,
             priority_models: priority_models
-          ).descendant_and_self_uris_partitioned.tap do |descendant_partitioned|
-            partitioned_uris[:priority].concat descendant_partitioned[:priority]
-            partitioned_uris[:other].concat descendant_partitioned[:other]
+          ).descendant_and_self_uris_partitioned_by_model.tap do |descendant_partitioned|
+            descendant_partitioned.keys.each do |k|
+              partitioned_uris[k] ||= []
+              partitioned_uris[k].concat descendant_partitioned[k]
+            end
           end
         end
         partitioned_uris
@@ -84,10 +105,7 @@ module CHF
         end
 
         def partitioned_uris
-          @partitioned_uris ||= {
-            priority: [],
-            other: []
-          }
+          @partitioned_uris ||= {}
         end
 
         def rdf_graph_models
@@ -96,15 +114,10 @@ module CHF
           end.compact
         end
 
-        def prioritized_object?
-          priority_models.present? && (rdf_graph_models & priority_models).count > 0
-        end
-
         def add_self_to_partitioned_uris
-          if prioritized_object?
-            partitioned_uris[:priority] << rdf_resource.subject
-          else
-            partitioned_uris[:other] << rdf_resource.subject
+          rdf_graph_models.each do |model|
+            partitioned_uris[model] ||= []
+            partitioned_uris[model] << rdf_resource.subject
           end
         end
     end
