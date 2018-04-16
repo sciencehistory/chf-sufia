419499087186a16e1196d80d53bd835508fbc12a
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-04-16 15:19:03 -0400

    oai-pmh integration test

---
+++ spec/features/oai_pmh_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+# Because creating real data is so slow, and feature tests pretty slow too,
+# we try to do everything in one test, even though that's not great test design.
+RSpec.feature "OAI-PMH feed", js: false do
+  before do
+    # no fits on travis
+    allow_any_instance_of(CharacterizeJob).to receive(:perform).and_return(nil)
+  end
+
+  let!(:work) { FactoryGirl.create(:public_work, :with_complete_metadata, :real_public_image) }
+  let!(:collection) { FactoryGirl.create(:public_collection) }
+  let(:oai_pmh_xsd_path) { Rails.root + "spec/fixtures/xsd/OAI-PMH.xsd" }
+
+  it "renders feed with just work" do
+    visit(oai_pmh_oai_path(verb: "ListRecords", metadataPrefix: "oai_dc"))
+
+    expect(page.status_code).to eq 200
+
+    # parse strict, so we get an exception if it's not well-formed XML
+    xml = Nokogiri::XML(page.body) { |config| config.strict }
+
+    # validate XSD
+    schema = Nokogiri::XML::Schema(File.read(oai_pmh_xsd_path))
+    errors = schema.validate(xml)
+    # PA Digital is at present asking for a dc:identifier.thumbnail element which
+    # fails validation.
+    expect(
+      errors.count == 0 ||
+      ( errors.count == 1 && errors.first.message.include?("identifier.thumbnail': This element is not expected.") )
+    )
+
+    # includes one item, which is the work, not the collection
+    records = xml.xpath("//oai:record", oai: "http://www.openarchives.org/OAI/2.0/")
+    expect(records.count).to eq (1)
+    record_id = records.first.at_xpath("./oai:header/oai:identifier", oai: "http://www.openarchives.org/OAI/2.0/")
+    expect(record_id.text).to eq("oai:sciencehistoryorg:#{work.id}")
+  end
+end
