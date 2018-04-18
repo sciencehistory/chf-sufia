fb3b73881a6361567b7d1064326c775282a837ac
Eddie Rubeiz <erubeiz@sciencehistory.org>
2018-04-18 16:17:02 -0400

    Rewrote the test and moved it to the right place.

--- spec/presenters/curation_concerns/generic_work_show_presenter_spec.rb
+++ spec/presenters/curation_concerns/generic_work_show_presenter_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+
+
 describe CurationConcerns::GenericWorkShowPresenter do
   let(:request) { double }
 
@@ -40,6 +42,23 @@ describe CurationConcerns::GenericWorkShowPresenter do
       expect(presenter.date_of_work_models.collect {|m| m.to_json(except: :id) }).to eq(dates_of_work_models.collect {|m| m.to_json(except: :id) })
       expect(presenter.date_of_work_models.all? {|m| m.readonly? }).to be true
     end
+  end
+
+  describe "display dates" do
+    let(:dates_of_work_models) { date_array }
+    let(:solr_document) { SolrDocument.new(work.to_solr) }
+    let(:work) do
+      FactoryGirl.build(:generic_work, title: ["work"],
+        dates_of_work: dates_of_work_models
+      )
+    end
+    it 'correctly displays the date strings given a set of date objects' do
+      the_display_dates = presenter.display_dates
+      expected_dates = correct_results
+      correct_results.each_with_index.map { |x,i| expect(expected_dates[i]).to eq x }
+      #puts the_display_dates
+    end
+
   end
 
 
@@ -90,4 +109,52 @@ describe CurationConcerns::GenericWorkShowPresenter do
       end
     end
   end
+end
+
+def date_array
+  data = [
+    ["1800",       "",            "",          "",       ""            ],
+    [nil,          nil,           nil,         nil,      nil           ],
+    [nil,          nil,           nil,         nil,      "circa"       ],
+    ["1912",       "",            "decade",    "",       ""            ],
+    ["1780",       "",            "decade",    "",       ""            ],
+    ["way back when",  "",        "decade",    "",       ""            ],
+    ["1912",       "",            "century",   "",       ""            ],
+    ["1780",       "",            "century",   "",       ""            ],
+    ["way back when",  "",        "century",   "",       ""            ],
+    ["1700",       "",            "century",   "",       ""            ],
+    ["the end of time",   "",     "after",     "",       "For real!"   ],
+    ["the end of time",   "",     "circa",     "",       ""            ],
+    ["1800",       "1900",        "century",   "",       "Note 1"      ],
+    ["1800",       "1900",        "century",   "",       "Note 2"      ],
+    ["1929-01-02", "1929-01-03",  "circa",     "before", "Note 3"      ],
+    ["1872",       "1929-01-03",  "after",     "before", "Note 4"      ],
+    ["1920",       "1928-11",     "decade",    "",       "Note 5"      ],
+  ]
+  data.collect { |d| DateOfWork.new(
+    start:           d[0],    finish:           d[1],
+    start_qualifier: d[2],    finish_qualifier: d[3],
+    note: d[4])}
+end
+
+def correct_results
+  [
+    "1800",
+    "",
+    " (circa)",
+    "Decade starting 1912",
+    "1780s",
+    "Decade starting way back when",
+    "Century starting 1912",
+    "Century starting 1780",
+    "Century starting way back when",
+    "1700s",
+    "After the end of time (For real!)",
+    "Circa the end of time",
+    "1800s – 1900 (Note 1)",
+    "1800s – 1900 (Note 2)",
+    "Circa 1929-Jan-02 – before 1929-Jan-03 (Note 3)",
+    "After 1872 – before 1929-Jan-03 (Note 4)",
+    "1920s – 1928-Nov (Note 5)"
+  ]
 end
