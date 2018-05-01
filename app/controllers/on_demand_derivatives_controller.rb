0e87fcdd32238759d78225033f6047e2c6b46ff4
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-05-01 11:37:16 -0400

    extract on demand deriv processing from controller
    makes specs more straightforward
    
    all these model class methods should maybe be extracted to a helper object. but good enough for now.

--- app/controllers/on_demand_derivatives_controller.rb
+++ app/controllers/on_demand_derivatives_controller.rb
@@ -12,9 +12,7 @@ class OnDemandDerivativesController < ApplicationController
   # Returns a JSON hash with status of on-demand derivative, including a URL
   # if it's available now.
   def pdf
-    checksum = checksum_for_work
-
-    record = find_or_create_record(work_id, "pdf", checksum)
+    record = OnDemandDerivative.find_or_create_record(work_id, "pdf", work_presenter: presenter)
 
     render json: record.as_json(methods: (record.success? ? "url" : nil))
   end
@@ -32,44 +30,4 @@ class OnDemandDerivativesController < ApplicationController
       Ability.new(current_user)
     )
   end
-
-  # Finds or creates the status record, and also kicks off the bg job if creating the status
-  # record. Also if the status record is stale, will delete it and create a new one.
-  def find_or_create_record(id, type, checksum, retry_count: 0)
-    if retry_count > 10
-      # what the heck is going on? Let's keep us from infinitely doing it and taking up all the CPU
-      raise StandardError.new("Tried to find/create an OnDemandDerivative record too many times for work #{id}")
-    end
-
-    record = OnDemandDerivative.where(work_id: id, deriv_type: type).first
-    if record.nil?
-      record = OnDemandDerivative.create(work_id: id, deriv_type: type, status: :in_progress, checksum: checksum)
-      CreateWorkPdfJob.perform_later(record)
-    end
-
-    if ( record.in_progress? && (Time.now - record.updated_at) > OnDemandDerivative::STALE_IN_PROGRESS_SECONDS ) ||
-       ( record.error? && (Time.now - record.updated_at) > OnDemandDerivative::ERROR_RETRY_SECONDS ) ||
-       ( record.success? && (record.checksum != checksum || !record.file_exists? ))
-          # It's stale, delete it!
-          record.delete
-          # and try again
-          record = find_or_create_record(id, type, checksum, retry_count: retry_count + 1)
-    end
-
-    return record
-  rescue ActiveRecord::RecordNotUnique
-    # race condition, someone else created it, no biggy
-    return find_or_create_record(id, type, checksum, retry_count: retry_count + 1)
-  end
-
-  # We gotta get all it's representatives, and compile all their checksums, to make
-  # sure checksum changes if members have changed at all. This will be a couple
-  # Solr fetches.
-  def checksum_for_work
-    representative_checksums = presenter.public_member_presenters.collect(&:representative_checksum).compact
-
-    Digest::MD5.hexdigest(representative_checksums.join("-"))
-  end
-
-
 end
--- spec/requests/on_demand_derivatives_controller_spec.rb
+++ spec/requests/on_demand_derivatives_controller_spec.rb
@@ -16,11 +16,31 @@ RSpec.describe OnDemandDerivativesController, type: :request do
 
       expect(json["work_id"]).to eq work.id
       expect(json["status"]).to eq "in_progress"
-      expect(json["url"]).to be_kind_of String
 
       last = OnDemandDerivative.last
       expect(last).to be_present
       expect(last.work_id).to eq work.id
+    end
+  end
+
+  describe "finished processing" do
+    let!(:record) { OnDemandDerivative.find_or_create_record(work.id, "pdf").tap do |record|
+      record.update(status: "success")
+    end }
+
+    it "returns json" do
+      expect(CreateWorkPdfJob).not_to receive(:perform_later)
+
+      get on_demand_pdf_path(work.id)
+
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(200)
+
+      json = JSON.parse(response.body)
+
+      expect(json["id"]).to eq record.id
+      expect(json["status"]).to eq "success"
+      expect(json["url"]).to be_kind_of String
     end
   end
 
