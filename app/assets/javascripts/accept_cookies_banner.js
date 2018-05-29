683a07f9732ca38da6f67049612b719e6672d6fd
Eddie Rubeiz <erubeiz@sciencehistory.org>
2018-05-29 13:22:37 -0400

    removing a couple comments

--- app/assets/javascripts/accept_cookies_banner.js
+++ app/assets/javascripts/accept_cookies_banner.js
@@ -9,10 +9,6 @@ chf.set_up_accept_cookies_banner = function () {
     jQuery ('.i-accept-link').click(chf.user_accepts_our_cookies);
 }
 chf.cookies_already_accepted_by_user = function () {
-    // TODO:
-    // 1) consider adding a path and/or expiration date.
-    // 2) consider including jquery cookie library;
-    // may work more consistently across browsers.
     return document.cookie.match(/user_accepts_our_cookies=true/) != null;
 }
 chf.user_accepts_our_cookies = function() {
