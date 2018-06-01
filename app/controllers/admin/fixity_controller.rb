db662fdf82cab2cd6a877031f05dd0e45e53ff11
Eddie Rubeiz <erubeiz@sciencehistory.org>
2018-06-01 09:47:36 -0400

    Sundry tweaks before pushing to staging

--- app/controllers/admin/fixity_controller.rb
+++ app/controllers/admin/fixity_controller.rb
@@ -14,27 +14,24 @@ module Admin
       @recent_checksum_count = ChecksumAuditLog.where( "created_at > '#{@x_days_ago}'").count
 
 
-      @most_recent_check_date = ChecksumAuditLog.latest_checks.order('created_at').first.created_at
 
-      # Note: old audit logs are kept when FileSets are deleted from the repo, which leads to unexpected results here.
-      # We may want to amend the prune_history functionality so that when a
-      # FileSet is deleted, all ChecksumAuditLogs associated with it are removed.
+      @most_recent_check_date = ChecksumAuditLog.latest_checks.order('created_at').first.created_at
       @oldest_check_date      = ChecksumAuditLog.latest_checks.order('created_at').last.created_at
 
 
-      # Count of the total number of unique files with checks on file
+      # Count of the total number of unique FileSets with checks on file
       @unique_checked_files_count = ChecksumAuditLog.select(:file_set_id).distinct.count
 
+      # Total number of FileSets in the repo:
+      @total_file_sets = FileSet.count
 
-      # Are there any files that have *not* been checked? (commented out for now, as this is too slow to be practical.)
+      # Are there any files that have *not* been checked? (commented out as this is too slow to be practical.)
 
       # all_fileset_ids = FileSet.all.map{|fs| fs.id}
       # all_checked_filset_ids = ChecksumAuditLog.select(:file_set_id).distinct.map{|fs| fs.file_set_id}
       # unchecked_fileset_ids = (all_fileset_ids -  all_checked_filset_ids)
 
 
-      # Total number of FileSets in the repo:
-      @total_files = FileSet.count
       @failed_checks = ChecksumAuditLog.where( passed: false)
       @no_failed_checks = (@failed_checks.count == 0)
     end
