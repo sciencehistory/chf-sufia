64a21940523196b62efa6d20e34a6a5894bc74f0
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-09-04 13:12:07 -0400

    fetches need to be inside lock too
    yes, this means the lock stays out for longer, which is not good. But if you fetched the works outside the lock, they
    may be altered by someone else before we get to editing it, which will cause an error

--- app/jobs/fileset_to_work_completion_job.rb
+++ app/jobs/fileset_to_work_completion_job.rb
@@ -12,20 +12,21 @@ class FilesetToWorkCompletionJob < ActiveJob::Base
   queue_as :jobs_server
 
   def perform(parent_id, child_work_id:, file_set_id:)
-    # Should we be validating? We already validated in controller before kicking off
-    # job, that's probably good enough for at least our actual use cases.
+    parent_work, new_child_work = nil, nil
+    acquire_lock_for(parent_id) do
+      # Should we be validating? We already validated in controller before kicking off
+      # job, that's probably good enough for at least our actual use cases.
 
-    parent_work = GenericWork.find(parent_id)
-    new_child_work  = GenericWork.find(child_work_id)
-    file_set    = FileSet.find(file_set_id)
+      parent_work = GenericWork.find(parent_id)
+      new_child_work  = GenericWork.find(child_work_id)
+      file_set    = FileSet.find(file_set_id)
 
 
-    # figure out it's context in the parent, and swap em.
-    place_in_order = parent_work.ordered_members.to_a.find_index(file_set)
-    was_thumbnail = is_thumbnail?(parent_work, file_set)
-    was_representative = is_representative?(parent_work, file_set)
+      # figure out it's context in the parent, and swap em.
+      place_in_order = parent_work.ordered_members.to_a.find_index(file_set)
+      was_thumbnail = is_thumbnail?(parent_work, file_set)
+      was_representative = is_representative?(parent_work, file_set)
 
-    acquire_lock_for(parent_work.id) do
       # Detach the fileset from the parent ...
       MemberConversionController.remove_member_from_parent(parent_work, file_set)
       # and attach the child work in its place.
--- app/jobs/work_to_fileset_completion_job.rb
+++ app/jobs/work_to_fileset_completion_job.rb
@@ -11,24 +11,26 @@ class WorkToFilesetCompletionJob < ActiveJob::Base
   queue_as :jobs_server
 
   def perform(parent_id, child_work_id:)
-    parent_work = GenericWork.find(parent_id)
-    child_work  = GenericWork.find(child_work_id)
+    child_work = nil
+    acquire_lock_for(parent_id) do
+      parent_work = GenericWork.find(parent_id)
+      child_work  = GenericWork.find(child_work_id)
 
-    # Should we be validating? We already validated in controller before kicking off
-    # job, that's probably good enough for at least our actual use cases.
+      # Should we be validating? We already validated in controller before kicking off
+      # job, that's probably good enough for at least our actual use cases.
 
-    place_in_order = parent_work.ordered_members.to_a.find_index(child_work)
-    was_thumbnail = is_thumbnail?(parent_work, child_work)
-    was_representative = is_representative?(parent_work, child_work)
+      place_in_order = parent_work.ordered_members.to_a.find_index(child_work)
+      was_thumbnail = is_thumbnail?(parent_work, child_work)
+      was_representative = is_representative?(parent_work, child_work)
 
-    file_set = child_work.members.first
+      file_set = child_work.members.first
 
-    acquire_lock_for(parent_work.id) do
-        # Detach the child work from the parent ...
-        MemberConversionController.remove_member_from_parent(parent_work, child_work)
-        # and replace it with the fileset.
-        MemberConversionController.add_to_parent(parent_work, file_set, place_in_order, make_representative: was_representative, make_thumbnail: was_thumbnail)
-        parent_work.save!
+
+      # Detach the child work from the parent ...
+      MemberConversionController.remove_member_from_parent(parent_work, child_work)
+      # and replace it with the fileset.
+      MemberConversionController.add_to_parent(parent_work, file_set, place_in_order, make_representative: was_representative, make_thumbnail: was_thumbnail)
+      parent_work.save!
     end
     child_work.delete
   end
