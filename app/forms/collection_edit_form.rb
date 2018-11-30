656f5435740be2175a32ec0d7706cb6b5c15a531
Eddie Rubeiz <erubeiz@sciencehistory.org>
2018-11-30 15:30:06 -0500

    Allow HTML in collection descriptions. Sanitize w/ Loofah on collection form save and again on collection display.

--- app/forms/collection_edit_form.rb
+++ app/forms/collection_edit_form.rb
@@ -1,5 +1,6 @@
 class CollectionEditForm < Sufia::Forms::CollectionForm
   include HydraEditor::Form
+  include ::CollectionFormBehavior
 
   def primary_terms
     [:title, :description, :related_url]
