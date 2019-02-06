c3c80f52d9fa597bbfc7986de46d5aecd28dc31f
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-02-06 14:42:32 -0500

    better download URL and sha1 hash

--- app/exporters/file_set_exporter.rb
+++ app/exporters/file_set_exporter.rb
@@ -1,14 +1,28 @@
 class FileSetExporter < Exporter
   def edit_hash(h)
-    h['file_urls'] = file_urls
+    h['file_url'] = file_url
+    h['sha_1'] = sha_1
     h
   end
 
-  def file_urls()
-    [Rails.application.routes.url_helpers.download_url(target_item.id)]
+
+  # Fun fact: if you upload a new version of a file to a FileSet,
+  # the number of items in the_file_set.files array does NOT change.
+  # Instead, a reference to the latest version of the file
+  # replaces the single element in the array.
+
+  def file_url()
+    target_item.files.last.uri.to_s
+  end
+
+  def sha_1()
+    sha_1_string = target_item.files.last.file_hash.last.id
+    raise RuntimeError, "SHA-1 hash not found for FileSet #{target_item.id}" unless sha_1_string.start_with? 'urn:sha1:'
+    sha_1_string.sub(/^urn:sha1:/, '')
   end
 
   def self.exportee()
     return FileSet
   end
+
 end
