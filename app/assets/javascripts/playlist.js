56d9441bd4ebc60311b78f02ebce0829eb836172
Jonathan Rochkind <jrochkind@sciencehistory.org>
2019-06-04 11:52:39 -0400

    function renames

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -14,57 +14,61 @@ $( document ).ready(function() {
 	function ChfAudioPlaylist(playlistWrapper) {
 	  this.playlistWrapper = $(playlistWrapper);
 
-	  this.firstTrack 		 = this.da_select('track-listing')[0];
-	  this.audioElement    = this.da_select('audio-elem')[0];
+	  this.firstTrack 		 = this.findByRole('track-listing')[0];
+	  this.audioElement    = this.findByRole('audio-elem')[0];
 
 
-	  this.audioElement.onended = this.play_next_track.bind(this);
-	  this.playlistWrapper.on("click", "[data-role='play-link']", this.user_clicked_on_a_track.bind(this));
+	  this.audioElement.onended = this.playNextTrack.bind(this);
+	  this.playlistWrapper.on("click", "[data-role='play-link']", this.onTrackClick.bind(this));
 
-	  var first_track = this.da_select('track-listing')[0];
-		this.prepare_track_for_play(first_track);
+	  var first_track = this.findByRole('track-listing')[0];
+		this.loadTrack(first_track);
 
 	};
 
-	ChfAudioPlaylist.prototype.play_next_track = function() {
-		this.prepare_track_for_play($("[data-currently-selected='true']").next()[0]);
-		this.play_audio();
+	ChfAudioPlaylist.prototype.playNextTrack = function() {
+		this.loadTrack($("[data-currently-selected='true']").next()[0]);
+		this.playAudio();
 	};
 
-	ChfAudioPlaylist.prototype.user_clicked_on_a_track = function(ev) {
+	ChfAudioPlaylist.prototype.onTrackClick = function(ev) {
 		ev.preventDefault();
 		var the_track = $(ev.target).parent();
-		this.prepare_track_for_play(the_track);
-		this.play_audio();
+		this.loadTrack(the_track);
+		this.playAudio();
 	};
 
-	ChfAudioPlaylist.prototype.da_select = function(role) {
+	ChfAudioPlaylist.prototype.findByRole = function(role) {
 		return this.playlistWrapper.find("[data-role='" + role + "']");
 	};
 
-	ChfAudioPlaylist.prototype.play_audio = function() {
-		// See : https://stackoverflow.com/questions/9421505/switch-audio-source-with-jquery-and-html5-audio-tag
-		this.audioElement.pause();
-		this.audioElement.load();
+	ChfAudioPlaylist.prototype.playAudio = function() {
+		// See: https://stackoverflow.com/questions/9421505/switch-audio-source-with-jquery-and-html5-audio-tag
+		// oncanplaythrough in case audio isn't fully loaded yet when we call this.
 		this.audioElement.oncanplaythrough = this.audioElement.play();
 	};
 
-	ChfAudioPlaylist.prototype.prepare_track_for_play = function(track) {
+	ChfAudioPlaylist.prototype.loadTrack = function(track) {
 		// css (for styling)
-		this.da_select('track-listing').removeClass("currently-selected");
+		this.findByRole('track-listing').removeClass("currently-selected");
 		$(track).addClass("currently-selected");
 
 		// data attribute (for identifying the item).
-		// da_select('track-listing').data('currently-selected', false);
+		// findByRole('track-listing').data('currently-selected', false);
 		// $(track).data('currently-selected', true);
-		this.da_select('track-listing').attr('data-currently-selected', 'false');
+		this.findByRole('track-listing').attr('data-currently-selected', 'false');
 		$(track).attr('data-currently-selected', 'true');
 
-		this.da_select('current-track-label').html( $(track).data('title'));
-		this.da_select('mp3-download')[0].href = $(track).data('mp3Url');
-		this.da_select('original-download')[0].href = $(track).data('mp3Url');
-		this.da_select('audio-mp3-source' )[0].src = $(track).data('mp3Url');
-		this.da_select('audio-webm-source')[0].src = $(track).data('webmUrl');
+		this.findByRole('current-track-label').html( $(track).data('title'));
+		this.findByRole('mp3-download')[0].href = $(track).data('mp3Url');
+		this.findByRole('original-download')[0].href = $(track).data('mp3Url');
+		this.findByRole('audio-mp3-source' )[0].src = $(track).data('mp3Url');
+		this.findByRole('audio-webm-source')[0].src = $(track).data('webmUrl');
+
+		// Tell HTML audio to load new stuff
+		// See: https://stackoverflow.com/questions/9421505/switch-audio-source-with-jquery-and-html5-audio-tag
+		this.audioElement.pause();
+		this.audioElement.load();
 	};
 
 	$("[data-role='audio-playlist-wrapper']").each(function() {
